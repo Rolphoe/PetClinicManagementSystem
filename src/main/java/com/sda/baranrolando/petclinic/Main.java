a770763cb3090f4c539f04b3fcbfb8a6991eeba7
Baran Rolando <Rolphoe@gmail.com>
2022-10-01 13:29:35 +0300

    PCMS_4: Implement a way for the user to interact with the system.

--- src/main/java/com/sda/baranrolando/petclinic/Main.java
+++ src/main/java/com/sda/baranrolando/petclinic/Main.java
@@ -4,10 +4,36 @@ import com.sda.baranrolando.petclinic.controller.VetController;
 import com.sda.baranrolando.petclinic.repository.VetRepositoryImpl;
 import com.sda.baranrolando.petclinic.service.VetServiceImpl;
 import com.sda.baranrolando.petclinic.utils.SessionManager;
+import com.sda.baranrolando.petclinic.utils.UserOption;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        SessionManager.getSessionFactory();
         VetController vetController = new VetController(new VetServiceImpl(new VetRepositoryImpl()));
-        vetController.createVet();
+        UserOption userOption;
+        Scanner scanner = new Scanner(System.in);
+        do{
+            UserOption.displayAllOptions();
+            System.out.print("Please select an option: ");
+            int numericOption = scanner.nextInt();
+            userOption = UserOption.findByNumericOption(numericOption);
+            switch(userOption){
+                case ADD_VET:
+                    vetController.createVet();
+                    break;
+                case VIEW_ALL_VETS:
+                    System.out.println("Not implemented!");
+                    break;
+                case UNKNOWN:
+                    System.err.println("!INVALID OPTION SELECTED!");
+                    break;
+                case EXIT:
+                    System.out.println("Good-bye, have a nice day.");
+                    break;
+            }
+        }while(userOption != UserOption.EXIT);
+        SessionManager.shutDown();
     }
 }
---
+++ src/main/java/com/sda/baranrolando/petclinic/utils/UserOption.java
@@ -0,0 +1,42 @@
+package com.sda.baranrolando.petclinic.utils;
+
+public enum UserOption {
+    ADD_VET(1, "Add a new vet"),
+    VIEW_ALL_VETS(2, "View all vets"),
+    EXIT(99, "Exit application!"),
+    UNKNOWN(999999, "Invalid Option selected!");
+    private final int numericOption;
+    private final String displayValue;
+
+    UserOption(int numericOption, String displayValue) {
+        this.numericOption = numericOption;
+        this.displayValue = displayValue;
+    }
+
+    public static void displayAllOptions(){
+        System.out.println("");
+        for (UserOption value : values()) {
+            if(value != UNKNOWN){
+                System.out.println(value.numericOption + " -> " + value.displayValue);
+            }
+
+
+        }
+    }
+
+    public static UserOption findByNumericOption(int numericOption){
+        for (UserOption value : UserOption.values()){
+            if(numericOption == value.getNumericOption()){
+                return value;
+            }
+        }
+        return UNKNOWN;
+    }
+    public int getNumericOption(){
+        return numericOption;
+    }
+
+    public String getDisplayValue() {
+        return displayValue;
+    }
+}
